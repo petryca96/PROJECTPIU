c68c618b3aaae71c4e3e72ca49049f4623da7623
Babandeep Singh <er.babandeep@gmail.com>
2021-06-12 18:29:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+    return (
+        <div>
+            <div className="container text-center">
+                <h1>Create-React-Bootstrap-App</h1>
+                <a className="btn btn-link mb-1 mt-1" target="_blank" href="https://getbootstrap.com/docs/5.0/getting-started/introduction">Visit bootstrap</a>
+                <h3>SCSS Customize</h3>
+                <p>Kindly visit main.scss and check the comments or visit <a className="btn-light" target="_blank" href="https://getbootstrap.com/docs/5.0/customize/sass/">SCSS Customize</a></p>
+            </div>
+        </div>
+    );
+}
