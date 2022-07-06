6965f7f1391d5660423c0b8dc530a0c11b821fd0
petryca96 <y_yuga@yahoo.com>
2022-07-06 19:32:28 +0300

    Add files via upload

---
+++ components/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Formular from "../formular";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1> nu azi</h1>
+      </div>
+    );
+  }
+}
